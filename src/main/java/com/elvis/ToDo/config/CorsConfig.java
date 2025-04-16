98fb1a3c1fb5c84d1935eafc9009ff512283f4d4
Elvis Júnior <elvisjunior994@gmail.com>
2025-04-16 01:12:05 -0300

    Front-End CORS config

--- src/main/java/com/elvis/ToDo/config/CorsConfig.java
+++ src/main/java/com/elvis/ToDo/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // permite todos os endpoints
-                        .allowedOrigins("http://localhost:5173") // frontend
+                        .allowedOrigins("https://to-do-front-project.vercel.app/") // frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true); // se quiser usar autenticação
